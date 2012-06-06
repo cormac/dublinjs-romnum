de6f5f9b1a75ccc117c153129e0812b921623993
cormac <cormac@studiorua.ie>
2012-06-06 01:26:47 +0100

    moved to same dir to get autotest working

--- RomNum.js
+++ RomNum.js
@@ -1,24 +1,30 @@
-var RomNum = {
-
-  output: '',
-  map: {
+var RomNum = {};
+(function(RomNum){
+  var map = {
     1: 'I',
     4: 'IV',
     5: 'V',
     9: 'IX',
     10: 'X',
-  },
+  };
+  var keys = Object.keys(map);
 
-  convert: function(input) {
-    var Inumeral= "I";
-    var Vnumeral= "V";
-    this.input = input;
-    return this.converterLogic(Vnumeral, Inumeral);
-  },
 
-    
+  var convert = function(input) {
+    var output = '';
+    while (input > 0) {
+      output = output + map[1];
+      input = input - 1;
+    }
+    return output;
 
+  };
+
+  Number.prototype.toRoman = function() {
+    return convert(this.valueOf());
+  };
+
+  exports.RomNum = RomNum;
   
-}
+})(RomNum);
 
-exports.RomNum = RomNum;
